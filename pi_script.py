1e6f98df0d73ff79de5a26145122c51a202c071c
Angie <43783837+angiebyun@users.noreply.github.com>
2021-01-05 20:02:13 -0500

    firebase connection

--- pi_script.py
+++ pi_script.py
@@ -5,6 +5,10 @@ import camera_capture as cam
 from predictors.YOLOv3 import YOLOv3Predictor
 from utils import *
 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 torch.cuda.empty_cache()
 
@@ -20,6 +24,15 @@ model = YOLOv3Predictor(params=yolo_params)
 image = cv2.imread(image_path)
 detections = model.get_detections(image)
 print(detections)
+
+# Firebase connection
+cred = credentials.Certificate(r"/Users/angie/Downloads/Firebase_Connection/smartmirrai-c2051-firebase-adminsdk-pnq5k-19437db28a.json")
+# Access Firebase DB
+firebase_admin.initialize_app(cred, {'databaseURL': 'https://smartmirrai-c2051-default-rtdb.firebaseio.com/'})
+
+# Fetch and print firebase DB
+ref = db.reference('images')
+print(ref.get())
 
 # TODO
 # send detections to firebase

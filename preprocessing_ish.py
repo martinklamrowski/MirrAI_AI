3f0cd7ea35644c93e6d2f2e13e2c03b724652af7
martinklamrowski <martinklamrowski@hotmail.com>
2021-01-31 22:54:46 -0500

    Nigeria

--- preprocessing_ish.py
+++ preprocessing_ish.py
@@ -6,18 +6,20 @@ import os
 
 # arrange data in yolo format ish
 
-# with open(config.STYLESENSE_DIR + "images.txt", "w") as file:
-#     for name in glob.glob(config.STYLESENSE_DIR + "data/*.jpg"):
-#
-#         name = os.path.split(name)[1]
-#         file.write(name + "\n")
+with open(config.STYLESENSE_DIR + "images.txt", "w") as file:
+    for name in glob.glob(config.STYLESENSE_DIR + "data/*.jpg"):
+
+        name = os.path.split(name)[1]
+        file.write(name + "\n")
 
 
 # fiftyone ish
-name = "stylesense"
-data_set = fo.Dataset.from_dir(
-    config.STYLESENSE_DIR, fo.types.YOLODataset, name=name
-)
-
-print(data_set)
-print(data_set.head())
+# name = "stylesense"
+# data_set = fo.Dataset.from_dir(
+#     config.STYLESENSE_DIR, fo.types.YOLODataset, name=name
+# )
+#
+# print(data_set)
+# print(data_set.head())
+#
+# session = fo.launch_app(dataset=data_set)

f88a293f1a3bdf17e491dc33bf927802676a86b1
martinklamrowski <martinklamrowski@hotmail.com>
2021-01-31 23:20:48 -0500

    Wow would be great to have more data.

--- preprocessing_ish.py
+++ preprocessing_ish.py
@@ -5,21 +5,21 @@ import os
 
 
 # arrange data in yolo format ish
-
-with open(config.STYLESENSE_DIR + "images.txt", "w") as file:
-    for name in glob.glob(config.STYLESENSE_DIR + "data/*.jpg"):
-
-        name = os.path.split(name)[1]
-        file.write(name + "\n")
+# with open(config.STYLESENSE_DIR + "images.txt", "w") as file:
+#     for name in glob.glob(config.STYLESENSE_DIR + "data/*.jpg"):
+#
+#         name = os.path.split(name)[1]
+#         file.write("data/" + name + "\n")
 
 
 # fiftyone ish
-# name = "stylesense"
-# data_set = fo.Dataset.from_dir(
-#     config.STYLESENSE_DIR, fo.types.YOLODataset, name=name
-# )
-#
-# print(data_set)
-# print(data_set.head())
-#
-# session = fo.launch_app(dataset=data_set)
+name = "stylesense"
+data_set = fo.Dataset.from_dir(
+    config.STYLESENSE_DIR, fo.types.YOLODataset, name=name
+)
+
+print(data_set)
+print(data_set.head())
+
+session = fo.launch_app(dataset=data_set, desktop=False, port=5151)
+session.wait()

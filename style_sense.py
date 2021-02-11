10cb3feae3e988854ecb9628c9548f39360a66c2
martinklamrowski <martinklamrowski@hotmail.com>
2021-02-10 19:54:47 -0500

    integration with UI ready

--- style_sense.py
+++ style_sense.py
@@ -29,57 +29,59 @@ def main():
         # allow the camera to warmup
         time.sleep(0.1)
 
-        for frame in camera.capture_continuous(
-                raw_capture, format="rgb", use_video_port=True
-        ):
-            # start_ms = time.time()
+        try:
+            for frame in camera.capture_continuous(
+                    raw_capture, format="rgb", use_video_port=True
+            ):
+                # start_ms = time.time()
 
-            raw_capture.truncate(0)
-            image = frame.array
+                raw_capture.truncate(0)
+                image = frame.array
 
-            _, scale = set_resized_input(
-                interpreter,
-                (cam_w, cam_h),
-                lambda size: cv2.resize(image, size),
-            )
-            interpreter.invoke()
+                _, scale = set_resized_input(
+                    interpreter,
+                    (cam_w, cam_h),
+                    lambda size: cv2.resize(image, size),
+                )
+                interpreter.invoke()
 
-            # get detections
-            detections = set()
-            objects = get_objects(interpreter, 0.5, scale)
+                # get detections
+                detections = set()
+                objects = get_objects(interpreter, 0.5, scale)
 
-            for obj in objects:
-                if labels and obj.id in labels:
-                    label_name = labels[obj.id]
-                    detections.add(label_name)
-                    caption = "{0}({1:.2f})".format(label_name, obj.score)
+                for obj in objects:
+                    if labels and obj.id in labels:
+                        label_name = labels[obj.id]
+                        detections.add(label_name)
+                        caption = "{0}({1:.2f})".format(label_name, obj.score)
 
-                    # bbox = (
-                    #     obj.bbox.xmin,
-                    #     obj.bbox.ymin,
-                    #     obj.bbox.xmax,
-                    #     obj.bbox.ymax,
-                    # )
+                        # bbox = (
+                        #     obj.bbox.xmin,
+                        #     obj.bbox.ymin,
+                        #     obj.bbox.xmax,
+                        #     obj.bbox.ymax,
+                        # )
 
-                    # print("{} - BBox: {}".format(caption, bbox))
-                    print("{}".format(caption))
-                print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+                        # print("{} - BBox: {}".format(caption, bbox))
+                        print("{}".format(caption))
+                    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
-            # fps ish
-            # elapsed_ms = time.time() - start_ms
-            # fps = 1 / elapsed_ms
-            # fps_text = "{0:.2f}ms, {1:.2f}fps".format((elapsed_ms * 1000.0), fps)
-            # print(fps_text)
+                # fps ish
+                # elapsed_ms = time.time() - start_ms
+                # fps = 1 / elapsed_ms
+                # fps_text = "{0:.2f}ms, {1:.2f}fps".format((elapsed_ms * 1000.0), fps)
+                # print(fps_text)
 
-            with open("detections.tmp", "w") as detections_file:
-                for d in detections:
-                    detections_file.write(d + " ")
-            os.rename("detections.tmp", "detections.txt")
-            time.sleep(2)
+                with open("detections.tmp", "w") as detections_file:
+                    for d in detections:
+                        detections_file.write(d + " ")
+                os.rename("detections.tmp", "detections.txt")
+                time.sleep(2)
 
-        with open("detections.txt") as _:
-            # just empty the file on quit
-            pass
+        finally:
+            with open("detections.txt") as _:
+                # just empty the file on quit
+                pass
 
 
 if __name__ == "__main__":

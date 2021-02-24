81adf62e8d24e2a5bee813771d3ed7d13990328a
martinklamrowski <martinklamrowski@hotmail.com>
2021-02-24 13:20:16 -0500

    Added some testing features to style_sense_edgetpu.py.

--- config/config.py
+++ config/config.py
@@ -1,4 +1,5 @@
 PATH_TO_PB_MODEL = "data/saved_model"
 PATH_TO_LABELS = "data/label_map.pbtxt"
+PATH_TO_TESTING_OUTPUT = "data/testing_output/"
 CAM_W = 640
 CAM_H = 640
--- style_sense_edgetpu.py
+++ style_sense_edgetpu.py
@@ -10,22 +10,27 @@ from pycoral.adapters.detect import get_objects
 from pycoral.utils.dataset import read_label_file
 from pycoral.utils.edgetpu import make_interpreter, run_inference
 
+import numpy as np
+
 import config.config as cfg
 
 
 def main():
-    default_model = "../stylesense/data/mobilenetv2_stylesense16_quant_int8_edgetpu.tflite"
-    default_labels = "../stylesense/data/stylesense_labels.txt"
+    model_path = "../stylesense/data/mobilenetv2_stylesense16_quant_int8_edgetpu.tflite"
+    labels_path = "../stylesense/data/stylesense_labels.txt"
 
     with picamera.PiCamera() as camera:
 
-        interpreter = make_interpreter(default_model)
+        interpreter = make_interpreter(model_path)
         interpreter.allocate_tensors()
-        labels = read_label_file(default_labels)
+        labels = read_label_file(labels_path)
 
         camera.resolution = (cfg.CAM_W, cfg.CAM_H)
-        camera.framerate = 10
+        camera.framerate = 0.5
         raw_capture = PiRGBArray(camera)
+
+        # image counter for testing
+        counter = 0
 
         # allow the camera to warmup
         time.sleep(0.1)
@@ -34,9 +39,6 @@ def main():
             for frame in camera.capture_continuous(
                     raw_capture, format="rgb", use_video_port=True
             ):
-                # start_ms = time.time()
-                time.sleep(2)
-
                 raw_capture.truncate(0)
                 image = frame.array
 
@@ -49,46 +51,42 @@ def main():
 
                 # get detections
                 detections = set()
-                objects = get_objects(interpreter, 0.5, scale)
+                objects = get_objects(interpreter, 0.75, scale)
 
                 for obj in objects:
                     if labels and obj.id in labels:
                         label_name = labels[obj.id]
                         detections.add(label_name)
-                        # caption = "{0}({1:.2f})".format(label_name, obj.score)
 
-                        # bbox = (
-                        #     obj.bbox.xmin,
-                        #     obj.bbox.ymin,
-                        #     obj.bbox.xmax,
-                        #     obj.bbox.ymax,
-                        # )
+                        # save images for testing
+                        # top-left corner
+                        tl_corner = (obj.bbox.xmin, obj.bbox.ymin)
 
-                        # print("{} - BBox: {}".format(caption, bbox))
-                        # print("{}".format(caption))
-                    # print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+                        # bottom-right corner
+                        br_corner = (obj.bbox.xmax, obj.bbox.ymax)
+
+                        color = tuple(np.random.random(size=3) * 256)
+                        thickness = 2
+
+                        image = cv2.rectangle(image, tl_corner, br_corner, color, thickness)
+                        cv2.putText(image, "{0}({1:.2f})".format(label_name, obj.score), tl_corner, cv2.FONT_HERSHEY_SIMPLEX, 0.8,
+                                    (255, 255, 255), 2, cv2.LINE_AA)
+
+                        cv2.imwrite("{}.jpg".format(counter), image)
+                        counter += 1
+
                 if len(detections) == 0:
-                    # print("I see a: nothing :(", file=sys.stdout)
                     output = "I see a: nothing :("
                 else:
                     output = "I see a: "
                     for d in detections:
                         output += d + " "
-                    # print(output, file=sys.stdout)
-                # fps ish
-                # elapsed_ms = time.time() - start_ms
-                # fps = 1 / elapsed_ms
-                # fps_text = "{0:.2f}ms, {1:.2f}fps".format((elapsed_ms * 1000.0), fps)
-                # print(fps_text)
 
                 with open("../stylesense/detections.tmp", "w") as detections_file:
                     detections_file.write(output)
                 os.rename("../stylesense/detections.tmp", "../stylesense/detections.txt")
 
         finally:
-            # with open("detections.txt", "w") as detections_file:
-            #     # just empty the file on quit
-            #     detections_file.write(":)")
             pass
 
 
